7aef58abaf4c70dba1857815980953a2e303c89d
Brandon Jose Vargas Rueda <brandon.vargas@labcomp.unet.edu.ve>
2020-02-07 12:11:27 -0400

    Acepta El Reto

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void recursividad(int contador,int aux)
+        {
+
+            Console.WriteLine(aux);
+            
+            
+            if (aux % contador == 0 )
+            {
+                if (contador == 1)
+                {
+                    Console.WriteLine("ES POLIDIVISIBLE");
+                }
+                else
+                {
+                    recursividad(contador - 1, aux / 10);
+                }
+            }
+            else
+            {
+                Console.WriteLine("NO POLIDIVISIBLE");
+            }
+
+        }
+
+        static void poli(string a)
+        {
+
+            int aux = int.Parse(a);
+            int b;
+            int conta = a.Length;
+
+            recursividad(conta, aux);
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            string numero_p;
+
+            bool flag;
+
+
+            do
+            {
+                flag = false;
+
+
+                Console.WriteLine("Introduce un numero para saber si es polidivisible ");
+                numero_p = Console.ReadLine();
+                
+                if(int.Parse(numero_p)<0 || int.Parse(numero_p)>1000000000000000000)
+                {
+                    Console.WriteLine("Numero de entrada incorrecto");
+                    flag = true;
+                }
+                else
+                {
+                    Console.WriteLine("Numero de entreda valido");
+                }
+                
+
+            }while (flag);
+
+            poli(numero_p);
+
+        }
+    }
+}
